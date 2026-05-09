7d5ecd7c5afefd0c3a0948ab07b25c97e1238aa9
Robbi Nespu <robbinespu@gmail.com>
2026-05-09 09:12:10 +0800

    test: phpunit baseline with sqlite-in-memory test database

---
+++ tests/TestCase/SmokeTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase;
+
+use Cake\TestSuite\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testFrameworkBoots(): void
+    {
+        $this->assertSame('8.1', substr(PHP_VERSION, 0, 3) === '8.1' ? '8.1' : substr(PHP_VERSION, 0, 3));
+        $this->assertTrue(extension_loaded('gd'), 'GD must be available');
+        $this->assertTrue(extension_loaded('pdo_sqlite'), 'pdo_sqlite must be available for tests');
+    }
+}
